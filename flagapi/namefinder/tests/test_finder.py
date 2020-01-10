f88ee6db75b8687cb60f11de0d99d2e4bee4456c
Paulo Marques <paulo.marques@ubc.ca>
2020-01-10 12:27:01 -0800

    improving positions method, fixing tests

--- flagapi/namefinder/tests/test_finder.py
+++ flagapi/namefinder/tests/test_finder.py
@@ -3,15 +3,29 @@ import pytest
 from flagapi.namefinder.views import find_names, find_names_position, replace_names
 
 
+@pytest.fixture
+def mock_names(settings):
+    fn = settings.FIRST_NAMES
+    ln = settings.LAST_NAMES
+    nn = settings.NO_NAMES
+    settings.FIRST_NAMES = ["carlos", "james", "mary", "silvio"]
+    settings.LAST_NAMES = ["bond", "santos"]
+    settings.NO_NAMES = ["james"]
+    yield
+    settings.FIRST_NAMES = fn
+    settings.LAST_NAMES = ln
+    settings.NO_NAMES = nn
+
+
 @pytest.mark.parametrize("sentence,names",
                          [
                              ("Mary got a little lamb.", ["Mary"]),
                              ("Carlos is doing a good job.", ["Carlos"]),
-                             ("My name is Bond, James Bond.", ["James"]),
+                             ("My name is Bond, James Bond.", ["James", "Bond"]),
                              ("Silvio Santos is a famous anchor in Brazil.", ["Silvio", "Santos"]),
                          ]
                          )
-def test_find_names(sentence, names):
+def test_find_names(mock_names, sentence, names):
     names_found = find_names(sentence)
     assert len(names_found) == len(names), "Number of names found is different than the number of names expected"
     for name in names:
@@ -22,11 +36,11 @@ def test_find_names(sentence, names):
                          [
                              ("Mary got a little lamb.", [(0, 4)]),
                              ("Carlos is doing a good job.", [(0, 6)]),
-                             ("My name is Bond, James Bond.", [(17, 22)]),
+                             ("My name is Bond, James Bond.", [(11, 15), (17, 22), (23, 27)]),
                              ("Silvio Santos is a famous anchor in Brazil.", [(0, 6), (7, 13)]),
                          ]
                          )
-def test_names_position(sentence, positions):
+def test_names_position(mock_names, sentence, positions):
     positions_found = find_names_position(sentence)
     assert sorted(positions_found) == sorted(positions), "Name position is different that name position expected"
 
@@ -37,7 +51,7 @@ def test_names_position(sentence, positions):
                              ("Silvio Santos is famous in Brazil.", "XXXXXX XXXXXX is famous in Brazil."),
                          ]
                          )
-def test_replace_names(sentence, replace):
+def test_replace_names(mock_names, sentence, replace):
     assert replace_names(sentence) == replace, "Sentence with replaced name is different that the expected."
 
 
@@ -46,26 +60,56 @@ def test_replace_names(sentence, replace):
                              ("Carlos is doing a good job.", "------ is doing a good job."),
                          ]
                          )
-def test_replace_names_to_replace(sentence, replace):
+def test_replace_names_to_replace(mock_names, sentence, replace):
     assert replace_names(sentence, to_replace="-") == replace, \
         "Sentence with replaced name is different that the expected."
 
 
 @pytest.mark.parametrize("sentence,replace",
                          [
-                             ("My name is Bond, James Bond.", "My name is Bond, XXXXXXXXXX Bond."),
+                             ("My name is Bond, James Bond.", "My name is XXXXXXXXXX, XXXXXXXXXX XXXXXXXXXX."),
                          ]
                          )
-def test_replace_names_fixed_size(sentence, replace):
+def test_replace_names_fixed_size(mock_names, sentence, replace):
     assert replace_names(sentence, fixed_size=10) == replace, \
         "Sentence with replaced name is different that the expected."
 
 
 @pytest.mark.parametrize("sentence,replace",
                          [
-                             ("My name is Bond, James Bond.", "My name is Bond, .......... Bond."),
+                             ("My name is Bond, James Bond.", "My name is .........., .......... ..........."),
                          ]
                          )
-def test_replace_names_to_replace_and_fixed_size(sentence, replace):
+def test_replace_names_to_replace_and_fixed_size(mock_names, sentence, replace):
     assert replace_names(sentence, to_replace=".", fixed_size=10) == replace, \
+        "Sentence with replaced name is different that the expected."
+
+
+@pytest.mark.parametrize("sentence,replace",
+                         [
+                             ("My name is Bond, James Bond.", "My name is Bond, James Bond."),
+                         ]
+                         )
+def test_replace_mock_names_to_replace_and_fixed_size_1(mock_names, sentence, replace):
+    assert replace_names(sentence, last_names_enabled=False, no_names_enabled=True) == replace, \
+        "Sentence with replaced name is different that the expected."
+
+
+@pytest.mark.parametrize("sentence,replace",
+                         [
+                             ("My name is Bond, James Bond.", "My name is XXXX, James XXXX."),
+                         ]
+                         )
+def test_replace_mock_names_to_replace_and_fixed_size_2(mock_names, sentence, replace):
+    assert replace_names(sentence, last_names_enabled=True, no_names_enabled=True) == replace, \
+        "Sentence with replaced name is different that the expected."
+
+
+@pytest.mark.parametrize("sentence,replace",
+                         [
+                             ("My name is Bond, James Bond.", "My name is Bond, XXXXX Bond."),
+                         ]
+                         )
+def test_replace_mock_names_to_replace_and_fixed_size_3(mock_names, sentence, replace):
+    assert replace_names(sentence, last_names_enabled=False, no_names_enabled=False) == replace, \
         "Sentence with replaced name is different that the expected."
--- flagapi/namefinder/views.py
+++ flagapi/namefinder/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.conf import settings
 
 from flagapi.namefinder.exceptions import VariableNotSet, ParameterMissing
@@ -15,10 +17,12 @@ def get_no_names_pack():
     return getattr(settings, 'NO_NAMES', [])
 
 
-def find_names(sentence=None):
+def find_names(sentence=None, last_names_enabled=True, no_names_enabled=False):
     """
     Find names in a sentence based on a FIRST_NAMES file
     :param sentence: Sentence
+    :param last_names_enabled: boolean, if True add LAST_NAMES from FIRST_NAMES
+    :param no_names_enabled: boolean, if True remove NO_NAMES from FIRST_NAMES
     :return List of list of names
     """
     if not sentence:
@@ -29,7 +33,19 @@ def find_names(sentence=None):
 
     first_names = get_first_names_pack()
     if not first_names:
-        raise Exception(VariableNotSet, "Variable FIRST_NAMES not set in settings.py")
+        raise Exception(VariableNotSet, "Variable FIRST_NAMES is not set in settings.py")
+
+    if last_names_enabled:
+        last_names = get_last_names_pack()
+        if not last_names:
+            raise Exception(VariableNotSet, "Variable LAST_NAMES is not set in settings.py")
+        first_names = list(set(first_names).union(set(last_names)))
+
+    if no_names_enabled:
+        no_names = get_no_names_pack()
+        if not no_names:
+            raise Exception(VariableNotSet, "Variable NO_NAMES is not set in settings.py")
+        first_names = list(set(first_names).difference(set(no_names)))
 
     punctuation = '!@#$%^&*()_+<>?:.,;'
 
@@ -45,10 +61,12 @@ def find_names(sentence=None):
     return to_return
 
 
-def find_names_position(sentence=None):
+def find_names_position(sentence=None, last_names_enabled=True, no_names_enabled=False):
     """
     Find names position in a sentence based on a FIRST_NAMES file
     :param sentence: Sentence
+    :param last_names_enabled: boolean, if True add LAST_NAMES from FIRST_NAMES
+    :param no_names_enabled: boolean, if True remove NO_NAMES from FIRST_NAMES
     :return List of tuples (begin, end) position
     """
     if not sentence:
@@ -57,23 +75,28 @@ def find_names_position(sentence=None):
     if not isinstance(sentence, str):
         raise Exception(TypeError, "This method requires string as input")
 
-    names_found = find_names(sentence)
+    names_found = find_names(sentence, last_names_enabled=last_names_enabled, no_names_enabled=no_names_enabled)
 
     to_return = []
     for name in names_found:
-        begin = sentence.lower().index(name.lower())
-        end = begin + len(name)
-        to_return.append((begin, end))
+        begin_positions = [m.start() for m in re.finditer(name, sentence)]
+        for begin in begin_positions:
+            to_return.append((begin, begin + len(name)))
+        # begin = sentence.lower().index(name.lower())
+        # end = begin + len(name)
+        # to_return.append((begin, end))
 
     return to_return
 
 
-def replace_names(sentence=None, to_replace="X", fixed_size=0):
+def replace_names(sentence=None, to_replace="X", fixed_size=0, last_names_enabled=True, no_names_enabled=False):
     """
     Find names in a sentence based on a FIRST_NAMES file and replace them
     :param sentence: Sentence
     :param to_replace: Character to be used for replaced names
     :param fixed_size: If greater than 0 the name will be replaced by fixed_size * to_replace
+    :param last_names_enabled: boolean, if True add LAST_NAMES from FIRST_NAMES
+    :param no_names_enabled: boolean, if True remove NO_NAMES from FIRST_NAMES
     :return The same sentence with replaced names.
     """
     if not sentence:
@@ -82,11 +105,16 @@ def replace_names(sentence=None, to_replace="X", fixed_size=0):
     if not isinstance(sentence, str):
         raise Exception(TypeError, "This method requires string as input")
 
-    positions_found = find_names_position(sentence)
+    positions_found = find_names_position(sentence, last_names_enabled=last_names_enabled,
+                                          no_names_enabled=no_names_enabled)
 
+    words_to_do = set()
     for position in positions_found:
         begin, end = position
         word = sentence[begin: end]
+        words_to_do.add(word)
+
+    for word in words_to_do:
         if fixed_size > 0:
             size = fixed_size
         else:
